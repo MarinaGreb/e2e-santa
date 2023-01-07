d802bd104099e72003df70347e91d1709a70e10a
marinagrebneva <sokovets@outlook.com>
2023-01-07 20:59:40 +0100

    box members added

--- cypress/e2e/santa-box-create.cy.js
+++ cypress/e2e/santa-box-create.cy.js
@@ -1,54 +1,63 @@
 const users = require("../fixtures/users.json");
 const boxPage = require("../fixtures/pages/boxPage.json");
 const generalElements = require("../fixtures/pages/general.json");
-const dashboardPage = require("../fixtures/pages/dashboardPage.json");
 const invitePage = require("../fixtures/pages/invitePage.json");
-const inviteeBoxPage = require("../fixtures/pages/inviteeBoxPage.json");
-const inviteeDashboardPage = require("../fixtures/pages/inviteeDashboardPage.json");
+const dashBordPage = require("../fixtures/pages/dashBoardPage.json");
+
 import { faker } from "@faker-js/faker";
 
-describe("user can create a box and run it", () => {
-  //пользователь 1 логинится
-  //пользователь 1 создает коробку
-  //пользователь 1 получает приглашение
-  //пользователь 2 переходит по приглашению
-  //пользователь 2 заполняет анкету
-  //пользователь 3 переходит по приглашению
-  //пользователь 3 заполняет анкету
-  //пользователь 4 переходит по приглашению
-  //пользователь 4 заполняет анкету
-  //пользователь 1 логинится
-  //пользователь 1 запускает жеребьевку
+describe("User can create box and run it", () => {
+  //Пользователь 1 логинится
+  //Пользователь 1 создаёт коробку
+  //Пользователь 1 получает приглашение
+  //Пользователь 2 переходит по приглашению
+  //Пользователь 2 заполняет анкету о себе
+  //Пользователь 3 переходит по приглашению
+  //Пользователь 3 заполняет анкету
+  //Пользователь 4 переходит по приглашению
+  //Пользователь 4 заполняет анкету
+  //Пользователь 1 логинится
+  //Пользователь 1 запускает жеребьёвку
   let newBoxName = faker.word.noun({ length: { min: 5, max: 10 } });
-  let wishes = faker.word.noun() + faker.word.adverb() + faker.word.adjective();
   let maxAmount = 50;
   let currency = "Евро";
   let inviteLink;
+  let wishes =faker.word.noun() +" " +faker.word.adjective() +" " +faker.word.adverb();
 
   it("user logins and create a box", () => {
     cy.visit("/login");
-    cy.login(users.userAutor.email, users.userAutor.password);
+    cy.login(users.userAuthor.email, users.userAuthor.password);
+
     cy.contains("Создать коробку").click();
     cy.get(boxPage.boxNameField).type(newBoxName);
+    cy.get(boxPage.boxKeyField).then((key) => {
+      const boxKey = Cypress.$(key).val(); //Сохранение значения из поля Идентификатор в переменную boxKey
+      cy.log(boxKey);
+    });
     cy.get(generalElements.arrowRight).click();
+
     cy.get(boxPage.sixthIcon).click();
     cy.get(generalElements.arrowRight).click();
-    cy.get(boxPage.giftPriceToggle).check({ force: true });
-    cy.get(boxPage.maxAnount).type(maxAmount);
+
+    cy.get(boxPage.giftPriceToggel).check({ force: true });
+    // cy.get('.switch__toggle').click();
+    cy.get(boxPage.maxAmount).type(maxAmount);
     cy.get(boxPage.currency).select(currency);
     cy.get(generalElements.arrowRight).click();
+    cy.get(generalElements.layoutSelector).click("left"); //клик вне формы
     cy.get(generalElements.arrowRight).click();
-    cy.get(generalElements.arrowRight).click();
-    cy.get(dashboardPage.createdBoxName).should("have.text", newBoxName);
+    // cy.get(generalElements.arrowRight).click();
+
+    cy.get(dashBordPage.createdBoxName).should("have.text", newBoxName);
     cy.get(".layout-1__header-wrapper-fixed .toggle-menu-item span")
       .invoke("text")
       .then((text) => {
+        cy.log(text);
         expect(text).to.include("Участники");
         expect(text).to.include("Моя карточка");
         expect(text).to.include("Подопечный");
       });
   });
-
   it("add participants", () => {
     cy.get(generalElements.submitButton).click();
     cy.get(invitePage.inviteLink)
@@ -59,36 +68,40 @@ describe("user can create a box and run it", () => {
     cy.clearCookies();
   });
   it("approve as user1", () => {
-    cy.visit(inviteLink);
-    cy.get(generalElements.submitButton).click();
-    cy.contains("войдите").click();
+    cy.goToLoginForm(inviteLink); //переход по ссылке для приглашения,переход к форме авторизации 
     cy.login(users.user1.email, users.user1.password);
-    cy.contains("Создать карточку участника").should("exist");
-    cy.get(generalElements.submitButton).click();
-    cy.get(generalElements.arrowRight).click();
-    cy.get(generalElements.arrowRight).click();
-    cy.get(inviteeBoxPage.wishesInput).type(wishes);
-    cy.get(generalElements.arrowRight).click();
-    cy.get(inviteeDashboardPage.noticeForInvitee)
-      .invoke("text")
-      .then((text) => {
-        expect(text).to.contain("Это — анонимный чат с вашим Тайным Сантой");
-      });
+    cy.creatingUserCard(wishes);
+    cy.clearCookies();
+  });
+
+  it("approve as user2", () => {
+    cy.goToLoginForm(inviteLink); //переход по ссылке для приглашения,переход к форме авторизации 
+    cy.login(users.user2.email, users.user2.password);
+    wishes =faker.word.noun() +" " +faker.word.adjective() +" " +faker.word.adverb()
+    cy.creatingUserCard(wishes);
+    cy.clearCookies();
+  });
+
+  it("approve as user3", () => {
+    cy.goToLoginForm(inviteLink); //переход по ссылке для приглашения,переход к форме авторизации 
+    cy.login(users.user3.email, users.user3.password);
+    wishes =faker.word.noun() +" " +faker.word.adjective() +" " +faker.word.adverb()
+    cy.creatingUserCard(wishes);
     cy.clearCookies();
   });
 
   after("delete box", () => {
     cy.visit("/login");
-    cy.login(users.userAutor.email, users.userAutor.password);
+    cy.login(users.userAuthor.email, users.userAuthor.password);
     cy.get(
-      '.layout-1__header-wrapper-fixed > .layout-1__header > .header > .header__items > .layout-row-start > [href="/account/boxes"] > .header-item > .header-item__text > .txt--med'
+      '.layout-1__header-wrapper-fixed [href="/account/boxes"] > .header-item'
     ).click();
-    cy.get(":nth-child(1) > a.base--clickable > .user-card").first().click();
+    cy.get(".user-card").first().click();
     cy.get(
-      ".layout-1__header-wrapper-fixed > .layout-1__header-secondary > .header-secondary > .header-secondary__right-item > .toggle-menu-wrapper > .toggle-menu-button > .toggle-menu-button--inner"
+      ".layout-1__header-wrapper-fixed .toggle-menu-button--inner"
     ).click();
     cy.contains("Архивация и удаление").click({ force: true });
-    cy.get(":nth-child(2) > .form-page-group__main > .frm-wrapper > .frm").type(
+    cy.get(":nth-child(2) > .form-page-group__main .frm").type(
       "Удалить коробку"
     );
     cy.get(".btn-service").click();
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -26,9 +26,31 @@
 
 const loginPage = require("../fixtures/pages/loginPage.json");
 const generalElements = require("../fixtures/pages/general.json");
+const inviteeBoxPage = require("../fixtures/pages/inviteeBoxPage.json");
+const inviteeDashBoardPage = require("../fixtures/pages/inviteeDashBoardPage.json");
 
 Cypress.Commands.add("login", (userName, password) => {
   cy.get(loginPage.loginField).type(userName);
   cy.get(loginPage.passwordField).type(password);
   cy.get(generalElements.submitButton).click({ force: true });
+});
+
+Cypress.Commands.add("creatingUserCard", (wishes) => {
+  cy.contains("Создать карточку участника").should("exist");
+  cy.get(generalElements.submitButton).click();
+  cy.get(generalElements.arrowRight).click();
+  cy.get(generalElements.arrowRight).click();
+  cy.get(inviteeBoxPage.wishesInput).type(wishes);
+  cy.get(generalElements.arrowRight).click();
+  cy.get(inviteeDashBoardPage.noticeForInvitee)
+    .invoke("text")
+    .then((text) => {
+      expect(text).to.contain("Это — анонимный чат с вашим Тайным Сантой");
+    });
+});
+
+Cypress.Commands.add("goToLoginForm", (inviteLink) => {
+  cy.visit(inviteLink);
+  cy.get(generalElements.submitButton).click();
+  cy.contains("войдите").click();
 });
