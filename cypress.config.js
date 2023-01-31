c5965d6db394b23170bbdfd1b56dda71e6fde3e5
marinagrebneva <sokovets@outlook.com>
2023-01-31 19:00:33 +0100

    added allure for reports

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,9 @@ const createEsbuildPlugin =
 const addCucumberPreprocessorPlugin =
   require("@badeball/cypress-cucumber-preprocessor").addCucumberPreprocessorPlugin;
 
+  const allureWriter = require('@shelex/cypress-allure-plugin/writer');
+// import allureWriter from "@shelex/cypress-allure-plugin/writer";
+
 module.exports = defineConfig({
   e2e: {
     baseUrl: "https://staging.lpitko.ru",
@@ -14,11 +17,14 @@ module.exports = defineConfig({
       const bundler = createBundler({
         plugins: [createEsbuildPlugin(config)],
       });
-      
       on("file:preprocessor", bundler);
       addCucumberPreprocessorPlugin(on, config);
-
+      //on('file:preprocessor', webpackPreprocessor);
+      allureWriter(on, config);
       return config;
     },
+    env: {
+      allureReuseAfterSpec: true
+  }
   },
 });
